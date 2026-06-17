48138558d01a9d8143eacca6931dc957fe280e66
Chawn Neal <chawnneal@gmail.com>
2026-06-17 10:50:11 -0400

    implement envalid to clean env

--- src/app/lib/fetchImages.ts
+++ src/app/lib/fetchImages.ts
@@ -1,6 +1,7 @@
 // @ is import alias
 import type { ImagesResults } from "@/models/images";
 import { ImagesSchemaWithPhotos } from "@/models/images"
+import env from "./env"
 
 // Promise<ImagesResults> is the return type
 // alt+z wrap down
@@ -9,7 +10,7 @@ export default async function fetchImages(url: string): Promise<ImagesResults | 
     // could be undefined, use envalid
     const res = await fetch(url, {
       headers: {
-        Authorization: process.env.PEXELS_API_KEY
+        Authorization: env.PEXELS_API_KEY
       }
     })
 
@@ -18,7 +19,7 @@ export default async function fetchImages(url: string): Promise<ImagesResults | 
     const imagesResults: ImagesResults = await res.json()
 
     //note this is on the server, so you will see in the terminal, not the browser console.
-    console.log(imagesResults)
+    // console.log(imagesResults)
 
     // Parse data with Zod schema
     const parsedData = ImagesSchemaWithPhotos.parse(imagesResults)
