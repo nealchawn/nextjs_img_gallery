6f1e2d4c4f2358b25826959ae61ca660a2de5ae1
Chawn Neal <chawnneal@gmail.com>
2026-06-21 19:45:45 -0400

    build basic ImageComponent container. add remote pattern as perscribed by nextjs Image doc

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: "/Users/chawn.neal/code/sandbox/nextjs_img_gallery",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,6 @@
 import fetchImages from "../lib/fetchImages";
 import type { ImagesResults } from "@/models/images";
-
+import ImgContainer from "./ImgContainer";
 
 
 
@@ -22,7 +22,7 @@ export default async function Gallery() {
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
         {
           images.photos.map(photo => (
-            <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+            <ImgContainer key={photo.id} photo={photo} />
           ))
         }
     </section>
